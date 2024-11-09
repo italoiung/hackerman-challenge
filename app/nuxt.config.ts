dcff0bbe80ed762e24502310ecf29b51e60ba8c1
Ítalo Iung <italoiung@gmail.com>
2024-11-09 14:09:45 -0300

    feat: add google fonts module config

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,4 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: ['@nuxtjs/google-fonts'],
+
+  googleFonts: {
+    families: {
+      Lato: [400, 600, 700],
+    },
+  },
 });
